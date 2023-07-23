efef5e5d4b696202e0c86c16c121db4580877545
Chihab Rouibi <07cr17@gmail.com>
2023-07-23 13:42:56 -0300

    Some chanching in code.

---
+++ RecetteWebAppMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecetteWebAppMVC.Models;
+
+namespace RecetteWebAppMVC.Data
+{
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+            {
+            }
+            public DbSet<Recette> Categories { get; set; }
+        }
+
+}
---
+++ RecetteWebAppMVC/Models/Recette.cs
@@ -0,0 +1,17 @@
+﻿namespace RecetteWebAppMVC.Models
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    public class Recette
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Rank")]
+        [Range(1, 1000, ErrorMessage = "Display order must be between 1 and 1000 only.")]
+        public int DisplayOrder { get; set; }        
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
